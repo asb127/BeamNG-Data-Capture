bba4e29d35fd429a05fd48fa415a020b15efb770
asb127 <99213674+asb127@users.noreply.github.com>
2025-02-15 20:50:18 +0100

    New file for data capture functions + updated .gitignore
    .gitignore updated to avoid saving Python cache files into repository.

---
+++ src/data_capture.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime
+from beamngpy.sensors import Camera
+
+def create_output_dir():
+    # Create a directory to store the captured images
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    output_dir = os.path.join(os.path.expanduser('~'), 'Documents', 'BeamNG-Data-Capture', timestamp)
+    os.makedirs(output_dir, exist_ok=True)
+    return output_dir
+
+def create_frame_output_dir(output_dir, i):
+    # Create a subfolder for every frame
+    frame_dir = os.path.join(output_dir, f'frame_{i}')
+    os.makedirs(frame_dir, exist_ok=True)
+    return frame_dir
+
+def save_camera_data(camera, output_dir):
+    # Poll the camera sensor
+    sensor_data = camera.poll()
+
+    # Split the sensor data
+    color_image = sensor_data['colour']
+    depth_image = sensor_data['depth']
+    semantic_image = sensor_data['annotation']
+
+    # Remove alpha channel from color image
+    color_image = color_image.convert('RGB')
+
+    # Save the images to the output directory
+    color_image.save(os.path.join(output_dir, 'color.png'))
+    depth_image.save(os.path.join(output_dir, 'depth.png'))
+    semantic_image.save(os.path.join(output_dir, 'semantic.png'))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import os
-from datetime import datetime
 from beamngpy import BeamNGpy, Scenario, Vehicle
 from beamngpy.sensors import Camera
+
+import data_capture
 
 # Instantiate BeamNGpy instance connecting to the simulator on the given host and port
 bng = BeamNGpy('localhost', 25252)
@@ -15,7 +15,7 @@ scenario = Scenario('west_coast_usa', 'capture_scenario')
 # Create a vehicle to capture data from
 vehicle = Vehicle('ego', model='etk800')
 # Add it to our scenario at this position and rotation
-scenario.add_vehicle(vehicle, pos=(-720, 100, 120), rot_quat=(0, 0, 0.35, 0.90))
+scenario.add_vehicle(vehicle, pos=(-720, 100, 119), rot_quat=(0, 0, 0.35, 0.90))
 # Place files defining our scenario for the simulator to read
 scenario.make(bng)
 
@@ -28,10 +28,7 @@ vehicle.ai.set_mode('span')
 # Create camera sensors and attach them to the vehicle (Try combining into one?)
 sensor_camera = Camera(name='sensor_camera', bng=bng, vehicle=vehicle, pos=(0, -0.5, 1.5), resolution=(1280, 720), is_render_colours=True, is_render_annotations=True, is_render_depth=True)
 
-# Create a directory to store the captured images
-timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-output_dir = os.path.join(os.path.expanduser('~'), 'Documents', 'BeamNG-Data-Capture', timestamp)
-os.makedirs(output_dir, exist_ok=True)
+output_dir = data_capture.create_output_dir()
 
 try:
     # Capture 10 images, one every 10 seconds
@@ -51,25 +48,9 @@ try:
             # Unable to reconnect with simulation
             print('Connection to simulator reset. Stopping script.')
             break
-        
-        sensor_camera_data = sensor_camera.poll()
 
-        # Split camera data
-        color_image = sensor_camera_data['colour']
-        depth_image = sensor_camera_data['depth']
-        semantic_image = sensor_camera_data['annotation']
-
-        # Remove alpha channel from color image
-        color_image = color_image.convert('RGB') 
-        
-        # Create a subfolder for every frame
-        frame_dir = os.path.join(output_dir, f'frame_{i}')
-        os.makedirs(frame_dir, exist_ok=True)
-        
-        # Save the images to the subfolder
-        color_image.save(os.path.join(frame_dir, 'color.png'))
-        depth_image.save(os.path.join(frame_dir, 'depth.png'))
-        semantic_image.save(os.path.join(frame_dir,'semantic.png'))
+        frame_dir = data_capture.create_frame_output_dir(output_dir, i)
+        data_capture.save_camera_data(sensor_camera, frame_dir)
 except KeyboardInterrupt:
     # User stopped the simulation process
     print('Simulation stopped by user')
