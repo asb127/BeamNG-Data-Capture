db781e06bb5e3744f1125f693873a7b9b5ffc785
asb127 <99213674+asb127@users.noreply.github.com>
2025-03-05 00:05:08 +0100

    Delete custom_types.py file

--- src/custom_types.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, Dict, Tuple
-
-# Define common tuple types
-Float3 = Tuple[float, float, float]
-Quat = Tuple[float, float, float, float]
-Dimensions2D = Tuple[int, int]
-
-# Define common dictionary types
-StrDict = Dict[str, Any]
